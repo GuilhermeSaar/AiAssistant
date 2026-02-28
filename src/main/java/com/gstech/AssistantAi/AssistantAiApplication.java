6ddd904125b4b22c11ce1e7d1284f2ae8e3f98b2
Guilherme <guilhermesaar02@gmail.com>
2026-02-28 14:32:26 -0300

    chore: initial project

---
+++ src/main/java/com/gstech/AssistantAi/AssistantAiApplication.java
@@ -0,0 +1,13 @@
+package com.gstech.AssistantAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistantAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gstech/AssistantAi/AssistantAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gstech.AssistantAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
